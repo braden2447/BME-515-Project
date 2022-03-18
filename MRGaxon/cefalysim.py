98ae1754b04610cc833f80f575109934eec32a44
Braden Garrison <braden.garrison@duke.edu>
2022-03-18 15:18:31 -0400

    Got cefalysim file running using MRG class object with mostly correct values

--- MRGaxon/cefalysim.py
+++ MRGaxon/cefalysim.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 
 # other imports
 import numpy as np
+from mrg import MRG
 
 # load run controls
 h.load_file('stdrun.hoc')
@@ -19,55 +20,32 @@ h.load_file('stdrun.hoc')
 # set temp
 h.celsius = 37
 
+# create MRG fiber model
+fiber = MRG(5.7, 51)
+
 # MODEL SPECIFICATION
 # time params =========================================================================
-h.tstop =  4 # [ms]: simulation time
+h.tstop = 25 # [ms]: simulation time
 h.dt = 0.001 # [ms]: timestep
 
-# cell params =========================================================================
-Vo = -80 # [mV]: Vm @ rest for initializing membrane potential at start of simulation
-n_nodes = 51 # []: (int) number of sections, make this an odd number
-D = 12 # [um]: fiber diameter
-inl = 100*D # [um]: internodal length
-rhoa = 54.7 # [Ohm]: axoplasmic/axial resistivity
-cm = 2.5 # [uF/cm**2]
-L = 1.5 # [um]
-nseg = 1 # []: (int)
-g = 1/2000 # [S/cm**2] 
+# cell parameters defined in MRG model
 
 # material params =====================================================================
-sigma_e = 2e-4 # [S/mm]: extracellular medium resistivity
+sigma_e = 0.00043 # [S/mm]: extracellular medium resistivity (skin)
 
 # stim params =========================================================================
 delay = 1 # [ms]: start time of stim
-dur = .2# [ms]: pulse width of (monopolar) stim
-amp = 0.6 # [nA]: amplitude of (intracellular) stim object -- but we are applying this extracellular (negative cathodic, positive anodic)
-e2f = 1   # [mm]: electrode to fiber distance
+dur = 0.25 # [ms]: pulse width of (monopolar) stim
+amp = 5 # [mA (EC)/nA (IC)]: amplitude of stim object -- but we are applying this extracellular (negative cathodic, positive anodic)
+e2f = 3 # [mm]: electrode to fiber distance
 
 # MODEL INITIALIZATION
 # define nodes for cell =================================================================
-nodes = [h.Section(name=f'node[{i}]') for i in range(n_nodes)]
-# f-string (as shown) is my personal preference, but also 
-#   ='node[{}]'.format(i) OR 
-#   ='node[%d]' % i
+# defined in MRG model
+# nodes = [h.Section(name=f'node[{i}]') for i in range(n_nodes)]
 
 # insert extracellular/mechanisms part of the circuit ===================================
-# connect the nodes =====================================================================
-for node_ind, node in enumerate(nodes):
-    node.nseg = nseg
-    node.diam = 0.6*D
-    node.L = L
-    node.Ra = rhoa*((L+inl)/L) # left this in here since it is a fn(*other params)
-    node.cm = cm
-
-    node.insert('AXNODE')
-    node.insert('extracellular')
-
-    for seg in node:
-        #seg.pas.g = g
-        seg.extracellular.e = 0
-    if node_ind>0:
-        node.connect(nodes[node_ind-1](1))
+# Handled by MRG model
 
 # INSTRUMENTATION - STIMULATION/RECORDING
 # make dummy object to "host" the stim - this will make sense later ===================
@@ -79,9 +57,8 @@ e_stim_obj.amp = amp
 e_stim_obj.delay = delay
 
 
-
 # create neuron "vector" for recording membrane potentials =================================
-vol_mem = [h.Vector().record(sec(0.5)._ref_v) for sec in nodes]
+vol_mem = [h.Vector().record(sec(0.5)._ref_v) for sec in fiber.node]
 tvec = h.Vector().record(h._ref_t)
 
 # SIMULATION CONTROL
@@ -89,29 +66,26 @@ tvec = h.Vector().record(h._ref_t)
 def update_field():
     #print(h.t)
     phi_e = []
-    ### Use for biphasic stim
-
-    #if h.t <= (delay + dur/2):
-        #e_stim_obj.amp = amp
-    #else:
-        #e_stim_obj.amp = -1*amp
         
-    for node_ind, node in enumerate(nodes):
-        x_loc = 1e-3*(-(n_nodes-1)/2*inl + inl*node_ind) # 1e-3 [um] -> [mm]
-        r = np.sqrt(x_loc**2 + e2f**2) # [mm]
+    for node_ind, node in enumerate(fiber.node):
+        # Bipolar stimulus
+        x_loc1 = 1e-3 * (-(fiber.axonnodes - 1) / 2 * fiber.interlength + fiber.interlength * node_ind)  # 1e-3 [um] -> [mm]
+        x_loc2 = x_loc1 + 13  # Shifted 13 mm from first contact
+        r1 = np.sqrt(x_loc1 ** 2 + e2f ** 2)  # [mm]
+        r2 = np.sqrt(x_loc2 ** 2 + e2f ** 2)  # [mm]
+        phi_e.append((e_stim_obj.i / (4 * sigma_e * np.pi * r1)) + (-1 * e_stim_obj.i / (4 * sigma_e * np.pi * r2)))
+        node(0.5).e_extracellular = phi_e[node_ind]
 
         # ========== left these in so you can check out in debugger ==========
         # x_axon.append(x_loc) # centered at middle node for an odd # of nodes
         # r.append(np.sqrt(x_loc**2 + e2f**2)) # [mm]
-        phi_e.append(e_stim_obj.i/(4*sigma_e*np.pi*r))
-        node(0.5).e_extracellular = phi_e[node_ind]
 
 # time integrate with constant time step - this just defines method, called by proc advance() below
 def my_advance():
     update_field()
     h.fadvance()
 
-h.finitialize(Vo)
+h.finitialize(fiber.v_init)
 
 # this is somewhat of a "hack" to change the default run procedure in HOC ==================
 h(r"""
@@ -124,26 +98,10 @@ h.continuerun(h.tstop)
 
 # DATA POST PROCESSING / OUTPUT
 # plot things ==============================================================================
-print(vol_mem)
-plt.plot(tvec, vol_mem[50])
-plt.xlabel('time (ms)')
+# print(vol_mem)
+plt.plot(tvec, vol_mem[25])
+plt.xlabel('Time (ms)')
 plt.ylabel('Vm (mA)')
 plt.show()
 
 print('============ DONE ============')
-
-#plotting different nodes overlayed
-plt.figure()
-my_nodes = vol_mem[20:30]
-                
-for node in my_nodes:
-    plt.plot(tvec, node)
-plt.xlabel('Time (ms)')
-plt.ylabel('Vm (mA)')
-plt.title('Anodic Activation sites for nodes 21-30 (dur = 200 us)')
-plt.legend([21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
-plt.xlim(0.975,1.3)
-plt.ylim((-155, 0))
-plt.show()
-
-
